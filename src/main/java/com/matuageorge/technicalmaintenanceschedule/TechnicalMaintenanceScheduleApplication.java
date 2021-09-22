9a02a0d8904a6fd304e794b04bc054a8988aa834
matua <matuageorge@gmail.com>
2021-09-22 12:35:05 +0300

    init commit

---
+++ src/main/java/com/matuageorge/technicalmaintenanceschedule/TechnicalMaintenanceScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.matuageorge.technicalmaintenanceschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalMaintenanceScheduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechnicalMaintenanceScheduleApplication.class, args);
+    }
+
+}
