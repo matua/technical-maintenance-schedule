8923e39962880d585e25705cfa96b0f53008fda2
matua <matuageorge@gmail.com>
2021-10-09 15:12:15 +0300

    fixed authentication controller

--- src/main/java/com/matuageorge/technicalmaintenanceschedule/controller/UserController.java
+++ src/main/java/com/matuageorge/technicalmaintenanceschedule/controller/UserController.java
@@ -1,13 +1,10 @@
 package com.matuageorge.technicalmaintenanceschedule.controller;
 
-import com.matuageorge.technicalmaintenanceschedule.config.security.jwt.JwtProvider;
-import com.matuageorge.technicalmaintenanceschedule.dto.UserAuthRequestDto;
-import com.matuageorge.technicalmaintenanceschedule.exception.NotAuthorizedException;
 import com.matuageorge.technicalmaintenanceschedule.exception.NotFoundException;
 import com.matuageorge.technicalmaintenanceschedule.exception.ValidationException;
 import com.matuageorge.technicalmaintenanceschedule.model.User;
 import com.matuageorge.technicalmaintenanceschedule.service.UserService;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -19,12 +16,11 @@ import static com.matuageorge.technicalmaintenanceschedule.config.Utility.USERS;
 @CrossOrigin
 @RestController
 @Slf4j
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RequestMapping(ADMIN)
 public class UserController {
 
     private final UserService userService;
-    private final JwtProvider jwtProvider;
 
     @GetMapping(USERS + "/{page}/{pageSize}")
     public ResponseEntity<Page<User>> findAll(
@@ -44,28 +40,5 @@ public class UserController {
 
         userService.toggleUserStatusByUserId(userId);
         return ResponseEntity.ok().build();
-    }
-
-    @PutMapping("/auth")
-    public ResponseEntity<String> authenticateUser(@RequestBody UserAuthRequestDto userAuthRequestDto) throws ValidationException, NotFoundException, NotAuthorizedException {
-
-        log.info("Authorizing user with email: {} and password: {}",
-                userAuthRequestDto.getEmail(),
-                userAuthRequestDto.getPassword());
-
-        User foundUser = userService.findByEmailAndPassword(
-                userAuthRequestDto.getEmail(),
-                userAuthRequestDto.getPassword());
-
-        String token = jwtProvider.generateToken(
-                foundUser.getEmail(),
-                foundUser.getFirstName(),
-                foundUser.getLastName(),
-                foundUser.getActive().toString(),
-                foundUser.getOnDuty().toString(),
-                foundUser.getRole().toString(),
-                foundUser.getId());
-
-        return ResponseEntity.ok(token);
     }
 }
