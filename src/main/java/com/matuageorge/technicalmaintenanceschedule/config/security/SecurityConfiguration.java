eab6b075a665022142c122f82d8cf270a5281349
matua <matuageorge@gmail.com>
2021-10-09 11:55:38 +0300

    init setup

--- src/main/java/com/matuageorge/technicalmaintenanceschedule/config/security/SecurityConfiguration.java
+++ src/main/java/com/matuageorge/technicalmaintenanceschedule/config/security/SecurityConfiguration.java
@@ -1,22 +1,14 @@
 package com.matuageorge.technicalmaintenanceschedule.config.security;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 
-@Configuration
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .cors().and()
-                .httpBasic().disable()
-                .csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authorizeRequests()
-                .antMatchers("/**").permitAll();
+        http.
+                httpBasic().disable();
     }
 }
